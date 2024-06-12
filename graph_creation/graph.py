1a512c20b5345c868d6721a6b9fe05a7efec245b
Muhammad Azeem Haider <mh06858@st.habib.edu.pk>
2024-06-13 02:01:51 +0500

    Changes made to graph.py and csv changes

--- graph_creation/graph.py
+++ graph_creation/graph.py
@@ -1,8 +1,8 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 
-file_path = ".\\graph_data\\single_bols\\random_truncation\\output_goodpairs.csv"
-file_path2 = ".\\graph_data\\single_bols\\random_truncation\\output_tempo.csv"
+file_path = ".\\graph_data\\double_bols\\rank_based_selection_truncation\\output_goodpairs.csv"
+file_path2 = ".\\graph_data\\double_bols\\rank_based_selection_truncation\\output_tempo.csv"
 
 data = pd.read_csv(file_path)
 data2 = pd.read_csv(file_path2)
@@ -12,14 +12,26 @@ filtered_data2 = data2[data2['Generation'] <= 50]
 
 plt.figure(figsize=(10, 6))
 
-# plt.plot(filtered_data['Generation'], 100-filtered_data['Average_Fitness'], label='Average Fitness')
-plt.plot(filtered_data['Generation'], (100-filtered_data['Best_Fit']) * 8, label='Best Fitness of Good Pairs multiplied by 8')
+# For tempo
+plt.plot(filtered_data2['Generation'], filtered_data2['Average_Fitness'], label='Average Fitness')
 plt.plot(filtered_data2['Generation'], filtered_data2['Best_Fit'], label='Best Fitness of Tempo')
-# plt.plot(filtered_data['Generation'], 100-filtered_data['Worst_Fit'], label='Worst Fitness')
+plt.plot(filtered_data2['Generation'], filtered_data2['Worst_Fit'], label='Worst Fitness')
+
+# For Good Pairs
+# plt.plot(filtered_data['Generation'], 100-(filtered_data['Average_Fitness']), label='Average Fitness')
+# plt.plot(filtered_data['Generation'], 100-(filtered_data['Best_Fit']), label='Best Fitness of Good Pairs')
+# plt.plot(filtered_data['Generation'], 100-(filtered_data['Worst_Fit']), label='Worst Fitness')
+
+# Comparison
+# plt.plot(filtered_data2['Generation'], filtered_data2['Best_Fit'], label='Best Fitness of Tempo')
+# plt.plot(filtered_data['Generation'], (100-(filtered_data['Best_Fit']))*8, label='Best Fitness of Good Pairs multiplied by 8')
 
 plt.xlabel('Generation')
 plt.ylabel('Fitness Value')
-plt.title('Comparison of Fitness Optimization of Two Factors')
+plt.title('Fitness over generations of Tempo optimization of the Music') # For tempo
+# plt.title('Fitness over generations of Good Pairs optimization of the Music') # For good pairs
+# plt.title('Comparison of Fitness optimization of two factors') # For comparison
+
 
 plt.legend()
 plt.show()
